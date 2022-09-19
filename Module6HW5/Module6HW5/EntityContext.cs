e9d5e3ff3b7f102f97b87caaf8ee467ac7d052b5
Анастасія Глусенко <anastasiia.hlusenko@nure.ua>
2022-09-20 00:23:42 +0300

    1commit

---
+++ Module6HW5/Module6HW5/EntityContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module6HW5.Entity;
+
+namespace Module6HW5
+{
+    public class EntityContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        public DbSet<RoleUsers> RoleUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-EP3R97J;Initial Catalog=Module6HW5.Test;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Users>()
+                .ToTable("Users")
+                .HasKey(k => k.Id);
+
+            modelBuilder.Entity<Users>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            modelBuilder.Entity<Users>()
+                .Property(p => p.Password)
+                .IsRequired();
+
+
+            modelBuilder.Entity<RoleUsers>()
+                .ToTable("RoleUsers")
+                .HasKey(k => k.RoleId);
+
+            modelBuilder.Entity<RoleUsers>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(20);
+        }
+    }
+}
---
+++ Module6HW5/Module6HW5/Models/RegisterUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Module6HW5.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        public string UserName { get;set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get;set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Confirm Password")]
+        public string ConfirmPassword { get;set; }
+    }
+}
